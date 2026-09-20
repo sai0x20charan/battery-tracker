b1ed3876df5a380812f6e7923aa2f1fcb7931d5d
sai-charan2003 <saicharannallapaneni@gmail.com>
2026-09-20 19:30:35 -0400

    fix(wear): set uses-library required to false and bump to v0.0.2

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.charan.batterytracker"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1001
-        versionName = "0.0.1"
+        versionCode = 1002
+        versionName = "0.0.2"
         vectorDrawables {
             useSupportLibrary = true
         }
