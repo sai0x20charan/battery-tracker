c02501516265c279f1272f891e6ca57dd9c58b17
sai-charan2003 <saicharannallapaneni@gmail.com>
2026-09-21 11:07:27 -0400

    refactor(mobile): migrate base theme to Theme.AppCompat and remove material dependency

--- mobile/build.gradle.kts
+++ mobile/build.gradle.kts
@@ -112,7 +112,6 @@ kotlin {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
 
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(platform(libs.androidx.compose.bom))
